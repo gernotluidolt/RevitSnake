8999c32c2d87a0ad7b1bb1d2b3e1fc5f61d7ae06
Gernot Luidolt <104714865+gernotluidolt@users.noreply.github.com>
2024-08-24 13:40:13 +0200

    Initial commit

---
+++ RevitCommandTemplate/SampleCommand.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitCommandTemplate
+{
+    [Transaction(TransactionMode.ReadOnly)] // Change to TransactionMode.Manual if you need to modify the model
+    public class SampleCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Get the UIDocument and Document (if needed)
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // your code here
+            TaskDialog.Show("Revit", "Hello World!");
+
+            return Result.Succeeded;
+        }
+    }
+}
